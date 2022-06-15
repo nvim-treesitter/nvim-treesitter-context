f01f8baecdd4982a5cd4b7d032d6ab9df19cc20c
Saecki <tobiasschmitz2001@gmail.com>
2022-06-16 00:56:39 +0200

    feat: add java queries

--- lua/treesitter-context.lua
+++ lua/treesitter-context.lua
@@ -203,6 +203,56 @@ do
         last = { f'pattern' },
       },
     },
+    java = {
+      ['class_declaration'] = {
+        category = CATEGORY.CLASS,
+        last = { f'interfaces', f'superclasses', f'name' },
+      },
+      ['enum_declaration'] = {
+        category = CATEGORY.ENUM,
+        last = { f'name' },
+      },
+      ['interface_declaration'] = {
+        category = CATEGORY.INTERFACE,
+        last = { f'extends_interfaces', f'name' },
+      },
+      ['constructor_declaration'] = {
+        category = CATEGORY.METHOD,
+        last = { f'parameters' },
+      },
+      ['method_declaration'] = {
+        category = CATEGORY.METHOD,
+        last = { f'parameters' },
+      },
+      ['for_statement'] = {
+        category = CATEGORY.FOR,
+        last = { f'update', f'condition', f'init' },
+      },
+      ['enhanced_for_statement'] = {
+        category = CATEGORY.FOR,
+        last = { f'value' },
+      },
+      ['while_statement'] = {
+        category = CATEGORY.WHILE,
+        last = { f'condition' },
+      },
+      ['if_statement' ] = {
+        category = CATEGORY.IF,
+        last = { f'condition' },
+      },
+      ['switch_expression' ] = {
+        category = CATEGORY.SWITCH,
+        last = { f'condition' },
+      },
+      ['switch_block_statement_group' ] = {
+        category = CATEGORY.CASE,
+        last = { t'switch_label' },
+      },
+      ['switch_rule' ] = {
+        category = CATEGORY.CASE,
+        last = { t'switch_label' },
+      },
+    },
     javascript = {
       ['class_declaration'] = {
         category = CATEGORY.CLASS,
