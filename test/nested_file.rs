b05cd9cbb972f48b583b581615a046f814ccaca0
Saecki <tobiasschmitz2001@gmail.com>
2022-06-18 17:09:27 +0200

    test: topline mode

---
+++ test/nested_file.rs
@@ -0,0 +1,17 @@
+impl Foo {
+
+    fn bar(&self) {
+        if condition {
+
+
+            for i in 0..100 {
+
+
+            }
+        }
+    }
+}
+
+struct Foo {
+
+}
--- test/ts_context_spec.lua
+++ test/ts_context_spec.lua
@@ -88,4 +88,99 @@ describe('ts_context', function()
     ]]}
   end)
 
+  it('edit a file in topline mode', function()
+    exec_lua[[require'treesitter-context'.setup{
+      mode = 'topline',
+      max_lines = 2,
+    }]]
+    cmd('edit test/nested_file.rs')
+    feed'L'
+    feed'<C-e>'
+    -- screen:snapshot_util()
+    screen:expect{grid=[[
+      {1:impl}{2: Foo {                    }|
+          {4:fn} {5:bar}({7:&}{8:self}) {           |
+              {4:if} condition {        |
+                                    |
+                                    |
+                  {4:for} i {4:in} {8:0}..{8:100} { |
+                                    |
+                                    |
+                  }                 |
+              }                     |
+          }                         |
+      }                             |
+                                    |
+      {4:^struct} {5:Foo} {                  |
+                                    |
+                                    |
+    ]], attr_ids={
+      [1] = {foreground = Screen.colors.Brown, background = Screen.colors.Plum1, bold = true};
+      [2] = {background = Screen.colors.Plum1};
+      [3] = {foreground = Screen.colors.Cyan4, background = Screen.colors.Plum1};
+      [4] = {bold = true, foreground = Screen.colors.Brown};
+      [5] = {foreground = Screen.colors.Cyan4};
+      [6] = {bold = true, foreground = Screen.colors.Blue1};
+      [7] = {bold = true, foreground = Screen.colors.SeaGreen4};
+      [8] = {foreground = Screen.colors.Fuchsia};
+    }}
+
+    feed'<C-e>'
+    screen:expect{grid=[[
+      {2:    }{1:fn}{2: }{3:bar}{2:(}{7:&}{8:self}{2:)             }|
+      {2:        }{1:if}{2: condition {        }|
+                                    |
+                                    |
+                  {4:for} i {4:in} {9:0}..{9:100} { |
+                                    |
+                                    |
+                  }                 |
+              }                     |
+          }                         |
+      }                             |
+                                    |
+      {4:^struct} {5:Foo} {                  |
+                                    |
+      }                             |
+                                    |
+    ]], attr_ids={
+      [1] = {foreground = Screen.colors.Brown, bold = true, background = Screen.colors.LightMagenta};
+      [2] = {background = Screen.colors.LightMagenta};
+      [3] = {background = Screen.colors.LightMagenta, foreground = Screen.colors.Cyan4};
+      [4] = {foreground = Screen.colors.Brown, bold = true};
+      [5] = {foreground = Screen.colors.Cyan4};
+      [6] = {foreground = Screen.colors.Blue1, bold = true};
+      [7] = {foreground = Screen.colors.SeaGreen4, bold = true, background = Screen.colors.LightMagenta};
+      [8] = {background = Screen.colors.LightMagenta, foreground = Screen.colors.Magenta1};
+      [9] = {foreground = Screen.colors.Magenta1};
+    }}
+
+    feed'3<C-e>'
+    screen:expect{grid=[[
+      {2:        }{1:if}{2: condition {        }|
+      {2:            }{1:for}{2: i }{1:in}{2: }{7:0}{2:..}{7:100}{2: { }|
+                                    |
+                                    |
+                  }                 |
+              }                     |
+          }                         |
+      }                             |
+                                    |
+      {4:^struct} {5:Foo} {                  |
+                                    |
+      }                             |
+      {6:~                             }|
+      {6:~                             }|
+      {6:~                             }|
+                                    |
+    ]], attr_ids={
+      [1] = {background = Screen.colors.Plum1, bold = true, foreground = Screen.colors.Brown};
+      [2] = {background = Screen.colors.Plum1};
+      [3] = {background = Screen.colors.Plum1, foreground = Screen.colors.Cyan4};
+      [4] = {foreground = Screen.colors.Brown, bold = true};
+      [5] = {foreground = Screen.colors.Cyan4};
+      [6] = {foreground = Screen.colors.Blue1, bold = true};
+      [7] = {background = Screen.colors.Plum1, foreground = Screen.colors.Magenta};
+    }}
+  end)
 end)
