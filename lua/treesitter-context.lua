2f9f75d33158c0289c70cc1e0975af9d36ec16b2
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2022-10-14 01:43:20 -0700

    feat: dynamically throttle context calculation (#174)
    Co-authored-by: Steven Arcangeli <stevearc@stripe.com>

--- lua/treesitter-context.lua
+++ lua/treesitter-context.lua
@@ -471,16 +471,29 @@ local function get_parent_matches(max_lines)
 end
 
 local function throttle_fn(fn)
-  local running = false
-  return function()
-    if not running then
-      running = true
-      vim.defer_fn(function()
-        fn()
-        running = false
-      end, 50)
+  local recalc_after_cooldown = false
+  local cooling_down = false
+  local function wrapped()
+    if cooling_down then
+      recalc_after_cooldown = true
+    else
+      local start = vim.loop.hrtime()
+      fn()
+      local elapsed_ms = math.floor((vim.loop.hrtime() - start) / 1e6)
+      -- If this took < 2ms, we don't need a cooldown period. This prevents the context floats from flickering
+      if elapsed_ms > 2 then
+        cooling_down = true
+        vim.defer_fn(function()
+          cooling_down = false
+          if recalc_after_cooldown then
+            recalc_after_cooldown = false
+            wrapped()
+          end
+        end, 20)
+      end
     end
   end
+  return wrapped
 end
 
 
