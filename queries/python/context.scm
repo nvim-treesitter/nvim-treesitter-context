db2cdfff1575751129e29bf1fd1fa6c6e744fdde
Denis Cornehl <denis@cornehl.org>
2023-02-04 15:13:20 +0100

    updated python queries

--- queries/python/context.scm
+++ queries/python/context.scm
@@ -1,10 +1,46 @@
+(class_definition
+  body: (_) @context.end
+) @context
+
+(function_definition
+  body: (_) @context.end
+) @context
+
+(try_statement
+  body: (_) @context.end
+) @context
+
+(with_statement
+  body: (_) @context.end
+) @context
+
+(if_statement
+  consequence: (_) @context.end
+) @context
+
+(elif_clause
+  consequence: (_) @context.end
+) @context
+
+(case_clause
+  consequence: (_) @context.end
+) @context
+
+(while_statement
+  body: (_) @context.end
+) @context
+
+(except_clause
+  (block) @context.end
+) @context
+
+(match_statement
+  alternative: (_) @context.end
+) @context
 
 ([
   (for_statement)
-  (function_definition)
-  (class_definition)
-  (if_statement)
-  (elif_clause)
+  (finally_clause)
   (else_clause)
   (pair)
 ] @context)
