3e3e3e82827e3e602c35a72f01e2b6028b0b0cb1
Lewis Russell <lewis6991@gmail.com>
2023-02-04 13:13:30 +0000

    add php

---
+++ test/test.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * comment
+ */
+function foo($a, $b) {
+  //loop, between low & high
+  while ($a <= $b) {
+    // comment
+    $index = $low + floor(($high - $low) * $delta);
+    // comment
+    $indexValue = $a;
+    if ($indexValue === $a) {
+      // comment
+
+
+      $position = $index;
+      return (int) $position;
+    }
+    if ($indexValue < $key) {
+      // comment
+
+      $low = $index + 1;
+    }
+    if ($indexValue > $key) {
+      // comment
+      do {
+        // comment
+        echo "The number is: $x <br>";
+        $x++;
+
+
+
+
+      } while ($x <= 5);
+
+      for ($x = 0; $x <= 10; $x++) {
+        echo "The number is: $x <br>";
+
+
+
+
+
+
+
+
+
+
+
+      }
+
+
+
+      foreach ($colors as $value) {
+        echo "$value <br>";
+
+
+
+
+
+
+      }
+
+      $high = $index - 1;
+    }
+  }
+
+
+
+  //when key not found in array or array not sorted
+  return null;
+}
+
+class Fruit {
+
+
+
+
+ // comment
+
+
+
+
+}
