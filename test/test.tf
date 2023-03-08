fd87c786d35beca11190536f513d51b63b33ba21
Lewis Russell <lewis6991@gmail.com>
2023-03-08 22:06:29 +0000

    Support for @context.start

---
+++ test/test.tf
@@ -0,0 +1,31 @@
+
+locals {
+  input_deps = {
+    for slug, group in local.t2r_map :
+    slug => {
+      a = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      a = 1
+      a = 1
+      a = 1
+    }
+  }
+}
