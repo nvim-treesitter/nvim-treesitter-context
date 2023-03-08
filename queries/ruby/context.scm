74378a0cc4b4da8e0328eda67bccb724f178bab4
Cameron <Alleyria@gmail.com>
2023-03-08 14:53:59 +0100

    Add context queries for ruby

---
+++ queries/ruby/context.scm
@@ -0,0 +1,31 @@
+(class
+  (body_statement) @context.end
+) @context
+
+(module
+  (body_statement) @context.end
+) @context
+
+(method
+  (body_statement) @context.end
+) @context
+
+(if
+  (then) @context.end
+) @context
+
+(if
+  (else (_) @context.end)
+) @context
+
+(unless
+  (then) @context.end
+) @context
+
+(unless
+  (else (_) @context.end)
+) @context
+
+(_
+  (do_block (body_statement) @context.end)
+) @context
