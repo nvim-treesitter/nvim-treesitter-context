89957fe8d1383a488eb8a8672db2942bb59f20ed
Damian Senn <damian.senn@topaxi.codes>
2023-03-10 08:38:30 +0100

    feat: add typescriptreact support

---
+++ queries/typescriptreact/context.scm
@@ -0,0 +1,29 @@
+(interface_declaration
+  body: (_ (_) @context.end)
+) @context
+
+(class_declaration
+  body: (_ (_) @context.end)
+) @context
+
+(method_definition
+  body: (_ (_) @context.end)
+) @context
+
+(for_statement
+  body: (_ (_) @context.end)
+) @context
+
+(function_declaration
+  body: (_ (_) @context.end)
+) @context
+
+(if_statement
+  consequence: (_ (_) @context.end)
+) @context
+
+(jsx_element
+  (jsx_opening_element (_) (_) @context.end)
+  (_)
+  (_)
+) @context
