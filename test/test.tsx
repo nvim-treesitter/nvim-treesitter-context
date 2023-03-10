b163a4efb0ae31308cb789dd75e9a6fe237d783e
Damian Senn <damian.senn@topaxi.codes>
2023-03-10 15:10:38 +0100

    add tsx test example file

---
+++ test/test.tsx
@@ -0,0 +1,92 @@
+/** @jsx Object */
+
+interface User {
+  name: string;
+
+
+
+  id: number;
+
+
+
+
+}
+ 
+class UserAccount {
+  name: string;
+  id: number;
+
+
+
+ 
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+
+    for (let i = 0; i < 3; i++) {
+        console.log("hello");
+
+
+
+    }
+
+
+
+
+  }
+}
+
+
+function wrapInArray(obj: string | string[]) {
+  if (typeof obj === "string") {
+    return [obj];
+
+
+
+
+  }
+  return obj;
+}
+
+
+function MyComponent() {
+  return (
+    <main role="main">
+      <div>
+
+
+
+
+
+        <MyComponent />
+
+
+
+
+
+
+      </div>
+    </main>
+  );
+}
+
+const MyComponentExpression = Object(() => {
+  return (
+    <main role="main">
+      <div>
+
+
+
+
+
+        <MyComponent />
+
+
+
+
+
+
+      </div>
+    </main>
+  );
+})
