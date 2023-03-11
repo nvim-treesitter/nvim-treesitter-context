f35bc5473117771b3b95125eea1bf1d13a550139
Andy Bell <andy-bell101@users.noreply.github.com>
2023-03-11 22:55:52 +0000

    add java queries (#225)
    * add java queries
    
    * remove unused alternation in java
    
    * add example java test file

---
+++ queries/java/context.scm
@@ -0,0 +1,21 @@
+(if_statement
+  consequence: (_) @context.end
+) @context
+
+(method_declaration
+  body: (_) @context.end
+) @context
+
+(for_statement
+  body: (_) @context.end
+) @context
+
+(enhanced_for_statement
+  body: (_) @context.end
+) @context
+
+(class_declaration
+  body: (_) @context.end
+) @context
+
+(expression_statement) @context
