b9e2cb495baf5f7617ec0fd76031162a1850f2ca
Lewis Russell <lewis6991@gmail.com>
2023-03-11 10:00:14 +0000

    perf: implement cache for is_valid

---
+++ lua/treesitter-context/cache.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+---Memoize a function using hash_fn to hash the arguments.
+---@generic F: function
+---@param fn F
+---@param hash_fn fun(...): any
+---@return F
+function M.memoize(fn, hash_fn)
+  local cache = setmetatable({}, { __mode = 'kv' }) ---@type table<any,any>
+
+  return function(...)
+    local key = hash_fn(...)
+    if cache[key] == nil then
+      local v = fn(...) ---@type any
+      cache[key] = v ~= nil and v or vim.NIL
+    end
+
+    local v = cache[key]
+    return v ~= vim.NIL and v or nil
+  end
+end
+
+return M
