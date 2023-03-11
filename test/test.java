609757004d624b214d6ecc4aac62e589e58dbc6f
Andrew Bell <andrew.bell012276@gmail.com>
2023-03-11 22:01:24 +0000

    add example java test file

---
+++ test/test.java
@@ -0,0 +1,44 @@
+package mymod;
+
+import stuff1;
+import stuff2;
+
+@class_annot_1
+@class_annot_2
+public class MyClass {
+
+
+
+
+    @method_annot_1
+    @method_annot_2
+    public void my_method(int param) {
+
+
+
+
+        if (true) {
+
+
+
+
+            for (int i = 0; i < 10; i++) {
+                
+
+
+
+                for (int var : iterable) {
+                    
+
+
+
+                    System.
+
+
+
+                        out.println("a message");
+                }
+            }
+        }
+    }
+}
