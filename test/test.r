47e5e3688cc886602490a0b5b46b2e29107c8abe
Frank <fbearoff@users.noreply.github.com>
2023-03-19 20:38:48 -0400

    add R queries

---
+++ test/test.r
@@ -0,0 +1,82 @@
+# (for)
+for (x in 1:10) {
+
+
+
+
+
+  print(x)
+}
+
+# (while)
+i <- 1
+while (i < 6) {
+
+
+
+
+
+
+  print(i)
+  i <- i + 1
+}
+
+# (if)
+if (b > a) {
+
+
+
+
+
+  print("b is greater than a")
+} else if (a == b) {
+  print("a and b are equal")
+} else {
+  print("a is greater than b")
+}
+
+# (left_assignment)
+left_assign <- list("a",
+
+
+
+
+
+  "b")
+
+# (right_assignment)
+right_assign -> list("a",
+
+
+
+
+
+  "b")
+
+# (equals_assignment)
+equals_assign = list("a",
+
+
+
+
+
+  "b")
+
+
+# (binary)
+mtcars %>%
+
+
+
+
+  mt
+
+# (call)
+library(
+
+
+
+
+
+
+ggplot2)
