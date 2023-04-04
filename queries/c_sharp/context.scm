3e918e209d2f197ca234aca9a88e555d197a3da0
romamihalich <miheev.roma90@mail.ru>
2023-04-04 19:31:27 +0300

    feat: add c# support

---
+++ queries/c_sharp/context.scm
@@ -0,0 +1,70 @@
+(interface_declaration
+  body: (_) @context.end
+) @context
+
+(class_declaration
+  body: (_) @context.end
+) @context
+
+(enum_declaration
+  body: (_) @context.end
+) @context
+
+(struct_declaration
+  body: (_) @context.end
+) @context
+
+(record_declaration
+  body: (_) @context.end
+) @context
+
+(record_struct_declaration
+  body: (_) @context.end
+) @context
+
+(namespace_declaration
+  body: (_) @context.end
+) @context
+
+(constructor_declaration
+  body: (_) @context.end
+) @context
+
+(destructor_declaration
+  body: (_) @context.end
+) @context
+
+(method_declaration
+  body: (_) @context.end
+) @context
+
+(switch_statement
+  body: (_) @context.end
+) @context
+
+(for_each_statement
+  body: (_) @context.end
+) @context
+
+(for_statement
+  body: (_) @context.end
+) @context
+
+(if_statement
+  consequence: (_) @context.end
+) @context
+
+([
+  (do_statement)
+  (while_statement)
+] @context)
+
+(try_statement
+  body: (_) @context.end
+) @context
+
+(catch_clause
+  body: (_) @context.end
+) @context
+
+(finally_clause) @context
