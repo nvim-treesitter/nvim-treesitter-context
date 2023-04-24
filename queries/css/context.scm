5c279e5a6c5eecaf37ea2a544edefd0212273e7e
Nils <40796807+uchars@users.noreply.github.com>
2023-04-24 10:08:00 +0200

    feat: added css & scss queries (#244)
    * feat: keyframes
    
    * add zig queries
    
    * feat: scss rule_set
    
    * feat: media
    
    * feat: css and scss inherits
    
    * chore: update readme

---
+++ queries/css/context.scm
@@ -0,0 +1,10 @@
+(rule_set) @context
+([
+  (rule_set)
+  (keyframe_block)
+  (keyframes_statement)
+  (media_statement)
+  (supports_statement)
+  (at_rule)
+] @context)
+
---
+++ queries/scss/context.scm
@@ -0,0 +1,13 @@
+; inherits: css
+
+([
+  (mixin_statement)
+  (function_statement)
+  (each_statement)
+  (for_statement)
+  (while_statement)
+  (if_clause)
+  (else_if_clause)
+  (else_clause)
+] @context)
+
