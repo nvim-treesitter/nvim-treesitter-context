bf8e96b1d8f2f6c5680b120a3ea455960c50f439
Nils <40796807+uchars@users.noreply.github.com>
2023-04-29 22:41:13 +0200

    feat(go): multiline & switch-case

--- queries/go/context.scm
+++ queries/go/context.scm
@@ -1,9 +1,28 @@
 ([
-  (method_declaration)
-  (function_declaration)
   (var_declaration)
   (type_declaration)
   (import_declaration)
-  (if_statement)
   (const_declaration)
+  (select_statement)
 ] @context)
+
+(function_declaration
+  body: (block (_) @context.end)
+) @context
+
+(method_declaration
+  body: (block (_) @context.end)
+) @context
+
+(if_statement
+  consequence: (block (_) @context.end)
+) @context
+
+(for_statement
+  body: (block (_) @context.end)
+) @context
+
+(communication_case
+  communication: (_)
+  (_) @context.end
+) @context
---
+++ test/test.go
@@ -0,0 +1,103 @@
+import (
+    "errors"
+
+
+    "fmt"
+
+)
+
+func (r *rect) area(a int,
+b int) int {
+    return r.width * r.height
+
+
+
+
+
+}
+
+var b
+  ,c
+  ,d int = 1, 2
+
+func foo(a int,
+  b int) (int,
+  int) {
+
+    i := 1
+
+  select {
+    case msg1 := <-c1:
+      fmt.Println("received", msg1)
+    case msg2 := <-c2:
+
+
+
+
+      fmt.Println("received", msg2)
+  }
+
+
+  for n := 0;
+  n <= 5; n++ {
+    if num := 9;
+    num < 0 {
+      fmt.Println(num, "is negative")
+
+
+
+
+
+    } else if num < 10 {
+      fmt.Println(num, "has 1 digit")
+
+
+
+
+
+
+
+    } else {
+      fmt.Println(num, "has multiple digits")
+
+
+
+
+
+
+
+
+    }
+    fmt.Println(n)
+  }
+
+  switch i {
+    case 1:
+      fmt.Println("one")
+    case 2:
+      fmt.Println("two")
+    case 3:
+      fmt.Println("three")
+
+
+
+
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
