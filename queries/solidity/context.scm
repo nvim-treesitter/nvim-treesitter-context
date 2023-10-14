ef449a2aa89de152dee9332b53b5707bd966a0c9
Meet Mangukiya <meet@flamy.dev>
2023-10-14 19:24:18 +0530

    feat: add context for solidity

---
+++ queries/solidity/context.scm
@@ -0,0 +1,24 @@
+(contract_declaration) @context
+(library_declaration) @context
+(interface_declaration) @context
+
+(event_definition) @context
+(constructor_definition) @context
+(function_definition) @context
+(modifier_definition) @context
+(fallback_receive_definition) @context
+
+(block_statement) @context
+(if_statement) @context
+(emit_statement) @context
+(revert_statement) @context
+(try_statement) @context
+(catch_clause) @context
+(for_statement) @context
+(while_statement) @context
+(do_while_statement) @context
+
+(call_expression) @context
+(error_declaration) @context
+(struct_declaration) @context
+(enum_declaration) @context
