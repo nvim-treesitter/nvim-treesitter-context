8d11b3a9fac2e73ef1437f5c5edb2e893cccd914
Tony Le <spiritphyz@users.noreply.github.com>
2024-02-19 18:00:52 -0500

    feat: add Vue test file

---
+++ test/test.vue
@@ -0,0 +1,81 @@
+<template>
+  <main>
+
+
+
+
+
+    <ul v-for="(button, index) in buttonList">
+
+
+
+
+
+      <li :key="index">
+          {{ button.content }}
+
+
+
+
+
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script setup>
+export default {
+  data() {
+    return {
+
+
+
+
+      buttonList: [],
+    }
+  },
+  methods: {
+    handleClick(target = null) {
+      if (!target) {
+
+
+
+
+
+      } else {
+
+
+
+
+
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  align-items: center;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+
+
+
+
+
+  .title {
+    color: var(--color-blue);
+    font-size: 1.5rem;
+    font-weight: 200;
+    margin: 0;
+    padding: 1rem 0;
+
+
+
+
+
+  }
+}
+</style>
