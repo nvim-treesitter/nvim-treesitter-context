1e37be3517ab59cbbab50d780dfb042097324702
Tom Kirchner <git@halffull.org>
2024-02-23 15:44:53 -0800

    feat(rust): add more queries and tests

--- queries/rust/context.scm
+++ queries/rust/context.scm
@@ -1,10 +1,33 @@
-
+; standard if
 (if_expression
   consequence: (_ (_) @context.end)
 ) @context
 
+; standard else
 (else_clause
   (block (_)) @context.end
+) @context
+
+; let if  (its else is caught above)
+(let_declaration
+   (if_expression
+     (block (_))) @context.end
+) @context
+
+; let else
+(let_declaration
+  alternative: (block (_) @context.end)
+) @context
+
+; let (tuple) = (values)
+(let_declaration
+  (tuple_pattern (_))
+  (tuple_expression _) @context.end
+) @context
+
+; helps with long array definitions
+(let_declaration
+   (array_expression _) @context.end
 ) @context
 
 (match_expression
@@ -26,7 +49,7 @@
 (loop_expression
   body: (_ (_) @context.end)
 ) @context
-  
+
 (closure_expression
   body: (_ (_) @context.end)
 ) @context
@@ -47,10 +70,48 @@
   body: (_ (_) @context.end)
 ) @context
 
+(struct_expression
+  (type_identifier) @context.end
+) @context
+
+(union_item
+  body: (_ (_) @context.end)
+) @context
+
 (enum_item
   body: (_ (_) @context.end)
 ) @context
 
 (mod_item
   body: (_ (_) @context.end)
+) @context
+
+; extern
+(foreign_mod_item
+  body: (_ (_) @context.end)
+) @context
+
+(async_block
+  (block (_) @context.end)
+) @context
+
+(try_block
+  (block (_) @context.end)
+) @context
+
+(unsafe_block
+  (block (_) @context.end)
+) @context
+
+; function call site; helps with long parameter lists
+(call_expression
+  (arguments (_) @context.end)
+) @context
+
+(macro_invocation
+  (token_tree (_) @context.end)
+) @context
+
+(macro_definition
+  name: (_) @context.end
 ) @context
--- test/test.rs
+++ test/test.rs
@@ -20,12 +20,181 @@ impl Foo {
 
 
 
-                // comment
 
 
+
+                foo(async move {
+
+
+
+
+
+
+                    // comment
+
+
+
+
+
+
+                })
             }
         }
+
+
+
+
+
+        try {
+
+
+
+
+
+
+            let Foo::Bar {
+                texts,
+                values,
+            } = foo().bar() else {
+
+
+
+
+
+                let a = if b {
+
+
+
+
+
+                    // comment
+
+
+
+
+
+                } else {
+
+
+
+
+                    // comment
+
+
+
+
+                };
+            }
+        }
+
+
+
+
+        short_call_site(a, b);
+
+        long_call_site(
+            a,
+
+            b,
+
+            c,
+
+            d,
+
+            e,
+        );
+
+
+
+
+        macro_rules! x {
+
+
+            // comment
+            () => {};
+
+
+        }
+
+
+
+
+        x! {
+
+
+
+            // comment
+
+
+
+        }
+
+
+
+        unsafe {
+
+
+            *0  // run
+
+
+        }
+
+
+
+
+        let short_array = [];
+
+        let long_array = [
+            1,
+
+            2,
+
+            3,
+
+            4,
+        ];
+
+        let (short, tuple) = (1, 2);
+
+        let (
+            a,
+
+            rather,
+
+            long,
+
+            tuple,
+        ) = (
+            1,
+
+            2,
+
+            3,
+
+            4,
+        );
     }
+
+    let s = BigStruct {
+
+        a,
+
+        b,
+
+        c,
+
+        d,
+    };
+}
+
+pub extern "C" {
+
+
+
+    pub fn foobar(_: *u8);
+
+
+
 }
 
 struct Foo {
@@ -37,5 +206,15 @@ struct Foo {
     email: String,
 
     sign_in_count: u64,
+
+}
+
+union Bar {
+
+    a: u8,
+
+    b: u16,
+
+    c: u32,
 
 }
