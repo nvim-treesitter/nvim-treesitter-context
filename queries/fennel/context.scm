4fe0a54e86859744968e1a5c7867b49c86855774
Alexei Mozaidze <lexerpexer@proton.me>
2024-04-24 20:46:52 +0400

    feat(fennel): improve queries

--- queries/fennel/context.scm
+++ queries/fennel/context.scm
@@ -1,6 +1,21 @@
 [
+  (quote_reader_macro)
+  (quasi_quote_reader_macro)
+  (quote_form)
   (fn_form)
   (lambda_form)
-  (hashfn_form)
-  (table)
+  (macro_form)
+  (case_form)
+  (match_form)
+  (case_try_form)
+  (match_try_form)
 ] @context
+
+(list
+  call: (symbol) @_sym
+  (#eq? @_sym "when")) @context
+
+(hashfn_reader_macro
+  expression: (list
+    call: (symbol) @_sym
+    (#eq? @_sym "do"))) @context
