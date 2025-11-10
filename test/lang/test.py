565e49b95e4c84a6af2be496c4e700f7ed7f41c9
Lewis Russell <lewis6991@gmail.com>
2025-11-10 10:28:27 +0000

    fix(python): exclude comments from function contexts

--- test/lang/test.py
+++ test/lang/test.py
@@ -1,6 +1,18 @@
+# {{TEST}}
 def hello(name: str, age: int) -> None: # {{CONTEXT}}
     print(f"Hello {name}! You are {age} years old.")
 
 
 
     # {{CURSOR}}
+
+# {{TEST}}
+def hello2(                # {{CONTEXT}}
+        name: str,         # {{CONTEXT}}
+        age: int) -> None: # {{CONTEXT}}
+    # comment
+
+
+
+
+    name = 'barry' # {{CURSOR}}
