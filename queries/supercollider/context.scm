13afabb5d36d059011e7c64f0dbe1188728db064
elgiano <elgiano@gmail.com>
2026-02-20 15:41:52 +0100

    feat(supercollider): added supercollider support

---
+++ queries/supercollider/context.scm
@@ -0,0 +1,15 @@
+[
+  (function_block)
+  (binary_expression)
+  (collection)
+  (indexed_collection)
+  (parameter_call_list)
+  (function_call)
+  (class_def)
+  (classvar)
+  (const)
+  (instance_var)
+  (variable_definition)
+  (variable_definition_sequence)
+  (code_block)
+] @context
